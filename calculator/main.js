112d78205e89bc2a84fc90a058b8efeed5dac0a2
hamoudsa <halqahta2007@my.fit.edu>
2018-11-21 09:21:30 +0300

    update solution

--- calculator/main.js
+++ calculator/main.js
@@ -1,36 +1,38 @@
+var squareButton = document.getElementById("square-button");
+
 function squareNumber(num) {
     var squaredNumber = num * num;
     console.log('' + num + ' is ' + squaredNumber);
     return squaredNumber;
 }
 
+var halfButton = document.getElementById("half-button");
 function halfNumber(num) {
     var halfNumber = num / 2;
     console.log('Half of ' + num + ' is ' +  halfNumber);
     return halfNumber;
 }
-
-function arOfCir(radius) {
-    var area = Math.PI * squareNumber(radius);
-    console.log('The area of circle with radius ' + radius + ' is ' + area);
-    return arOfCir;
+var areaInput = document.getElementById("area-input");
+var areaButton = document.getElementById("area-button");
+function arOfCir() {
+    
+    var area = Math.PI * squareNumber(areaInput.value);
+    console.log('The area of circle with radius ' + areaInput.value + ' is ' + area);
+    return area;
 }
     
-var squareButton = document.getElementById("square-button");
 squareButton.addEventListener("click", function() {
   var num = document.getElementById("square-input").value;
   document.getElementById("solution").innerHTML = squareNumber(num);
 });
     
-var halfButton = document.getElementById("half-button");
 halfButton.addEventListener("click", function() {
   var num = document.getElementById("half-input").value;
   document.getElementById("solution").innerHTML = halfNumber(num);
 });
     
 
-var areaButton = document.getElementById("area-button");
 areaButton.addEventListener("click", function() {
   var num = document.getElementById("area-input").value;
-  document.getElementById("solution").innerHTML = arOfCir(radius);
+  document.getElementById("solution").innerHTML = arOfCir();
 });
