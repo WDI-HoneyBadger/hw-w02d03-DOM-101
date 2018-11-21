f5ecc77ff550647a7df0adfa6962267ae406031c
Turki Alomari <t-matar@windowslive.com>
2018-11-21 09:03:04 +0300

    done homerwork

---
+++ modlips/main.js
@@ -0,0 +1,22 @@
+console.log('js connected!');
+var button = document.querySelector('#lib-button');
+console.log(button);
+var noun = document.querySelector('#noun');
+var adiective = document.querySelector('#adjective').value;
+var person = document.querySelector('#person');
+
+var noun= noun.value;
+console.log('noun value is', noun);
+
+// var adjective = adjective.value;
+console.log('adjective value is', adiective);
+
+var person = person.value;
+console.log('person value is', person);
+
+function makemadlib(){
+console.log. ('')
+
+}
+
+button.addEventListener("click" ,  makemadlib);
