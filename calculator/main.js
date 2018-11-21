12dbe9d30ba7c8eadd4164634c2f9d7f9d6e0bfd
hamad007 <eng_cis1@hotmail.com>
2018-11-21 05:08:40 +0300

    calculator_ madlips Done

---
+++ calculator/main.js
@@ -0,0 +1,76 @@
+
+
+var inputs = document.querySelector('#square-input');
+var buttons = document.querySelector('#square-button');
+var resultSections = document.querySelector('.form-result-s');
+
+function squareThisNumbers(){
+  console.log('clickity click');
+
+  var userInputs = inputs.value;
+  console.log('inputs value is ', userInputs);
+
+  var squareNumberInputs = Math.pow(userInputs, 2);
+console.log(squareNumberInputs);
+
+var newPTags = document.createElement('p');
+  var newPTagTexts = document.createTextNode(squareNumberInputs);
+
+  newPTags.appendChild(newPTagTexts);
+  resultSections.appendChild(newPTags);
+}
+
+buttons.addEventListener('click', squareThisNumbers);
+
+
+
+
+
+var inputh = document.querySelector('#half-input');
+var buttonh = document.querySelector('#half-button');
+var resultSectionh = document.querySelector('.form-result-h');
+
+function halfThisNumberh(){
+  console.log('clickity click');
+
+  var userInputh = inputh.value;
+  console.log('input value is ', userInputh);
+ 
+  var halfNumberInputh = (userInputh / 2);
+console.log(halfNumberInputh);
+
+var newPTagh = document.createElement('p');
+  var newPTagTexth = document.createTextNode(halfNumberInputh);
+
+  newPTagh.appendChild(newPTagTexth);
+  resultSectionh.appendChild(newPTagh);
+}
+
+buttonh.addEventListener('click', halfThisNumberh);
+
+
+
+
+
+
+var input = document.querySelector('#area-input');
+var button = document.querySelector('#area-button');
+var resultSection = document.querySelector('.solution');
+
+function calculateAreaCircle(){
+  console.log('clickity click');
+
+  var userInput = input.value;
+  console.log('input value is ', userInput);
+
+  var halfNumberInput = Math.pow(userInput,2)  * 3.14;
+console.log(halfNumberInput);
+
+var newPTag = document.createElement('p');
+  var newPTagText = document.createTextNode(halfNumberInput);
+
+  newPTag.appendChild(newPTagText);
+  resultSection.appendChild(newPTag);
+}
+
+button.addEventListener('click', calculateAreaCircle);
---
+++ madlips/main.js
@@ -0,0 +1,21 @@
+var list=document.getElementById("clom1");
+var items=document.getElementsByTagName("li");
+
+for(var i=0;i<=items.list;i++){
+    i=items.button;
+    console.log(i);
+}
+
+for (var i = 0; i < 10; i++){
+    console.log(i);
+}
+
+
+
+
+// function clicke(){
+//     console.log("hh");
+// }
+
+// var valuebutton=document.querySelector("#lib-button");
+// valuebutton.addEventListener("click",clicke);
